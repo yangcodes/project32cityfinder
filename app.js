25a98b1d6c85f4e6ae2fd10f53322caef92af50b
Jenny Lin <misslinyang91@gmail.com>
2021-09-02 17:56:41 +1000

    fetched the json data

---
+++ app.js
@@ -0,0 +1,19 @@
+const searchInput = document.querySelector(".search");
+const suggestionsContainer = document.querySelector(".suggestions");
+
+searchInput.addEventListener("change", displayMatches);
+searchInput.addEventListener("keyup", displayMatches);
+
+const citiesStates = [];
+
+fetch(
+  "https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json"
+)
+  .then((response) => response.json())
+  .then((responseData) => {
+    citiesStates.push(...responseData);
+    console.log(citiesStates);
+  });
+function displayMatches() {
+  console.log("meow");
+}
